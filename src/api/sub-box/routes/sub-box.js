f5c2a686d965830e75cfa1cbab0d40dac985cadb
travis-submachine <travis@sub-machine.co>
2024-06-17 14:09:02 +0100

    Routes Update

--- src/api/sub-box/routes/sub-box.js
+++ src/api/sub-box/routes/sub-box.js
@@ -1,9 +1,45 @@
-'use strict';
-
-/**
- * sub-box router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::sub-box.sub-box');
+{
+    "routes": [
+      {
+        "method": "GET",
+        "path": "/sub-boxes",
+        "handler": "sub-box.find",
+        "config": {
+          "policies": []
+        }
+      },
+      {
+        "method": "GET",
+        "path": "/sub-boxes/:id",
+        "handler": "sub-box.findOne",
+        "config": {
+          "policies": []
+        }
+      },
+      {
+        "method": "POST",
+        "path": "/sub-boxes",
+        "handler": "sub-box.create",
+        "config": {
+          "policies": []
+        }
+      },
+      {
+        "method": "PUT",
+        "path": "/sub-boxes/:id",
+        "handler": "sub-box.update",
+        "config": {
+          "policies": []
+        }
+      },
+      {
+        "method": "DELETE",
+        "path": "/sub-boxes/:id",
+        "handler": "sub-box.delete",
+        "config": {
+          "policies": []
+        }
+      }
+    ]
+  }
+  
